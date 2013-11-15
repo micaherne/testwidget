36bfa3912273e411c20a5cb469d44ad2fd71db71
Michael Aherne <michael.aherne@strath.ac.uk>
2013-11-15 10:10:20 +0000

    Added some documentation

--- WebContent/js/strath-widget.js
+++ WebContent/js/strath-widget.js
@@ -1,7 +1,22 @@
-function getFeatureApi(uri, local) {
-	if (typeof window.mpegasus !== 'undefined') {
-		return window.mpegasus.getFeatureApi(uri);
-	} else {
-		return local;
-	}
+/*
+ * This file would be included in all packages.
+ * 
+ * The getFeatureApi() function enables the use of a local service URL
+ * (i.e. when the client pages have been opened from the same server
+ * location as the service is hosted), but if there is an mpegasus
+ * object this will be allowed to manage access to features.
+ * 
+ * This is designed to ensure that the function will only be defined
+ * once, although this could cause issues if different widgets had 
+ * different versions of this function in the same page.
+ * 
+ */
+if (typeof window.getFeatureApi === 'undefined') {
+	window.getFeatureApi = function(uri, local) {
+		if (typeof window.mpegasus !== 'undefined') {
+			return window.mpegasus.getFeatureApi(uri);
+		} else {
+			return local;
+		}
+	};
 }
